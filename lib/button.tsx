b8b8c126abde5fe01421017f8fdef6196236716c
hedongsheng <he77505357@163.com>
2020-04-21 20:03:07 +0800

    引入react

---
+++ lib/button.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+function Button() {
+    return (
+        <h1>hello </h1>
+    )
+}
+
+export default Button
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,9 @@
-console.log('hi4')
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+
+import Button from './button'
+
+ReactDom.render(
+    <Button />,
+    document.getElementById('root')
+)
