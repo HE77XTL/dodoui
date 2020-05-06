befecc7bf45bdc303ac1240b2d8f0d99e24862fd
HE77XTL <he77505357@163.com>
2020-05-06 23:24:55 +0800

    icon 组件，自定义class 继承所有事件

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
-import "./importAllIcons"
-import "./icon.scss"
+import "./importAllIcons";
+import "./icon.scss";
+import classnames from "../helpers/classnames";
 
 // 静态引入可以 tree-shaking
 //import "../../icons/wechat.svg";
 //import '../../icons/qq.svg'
 
-interface iconProps {
+interface iconProps extends React.SVGAttributes<SVGElement> {
     name: string
 }
 
-const Icon: React.FunctionComponent<iconProps> = (props) => {
+const Icon: React.FunctionComponent<iconProps> = ({name, className, ...resetProps}) => {
     return (
         <span>
-            <svg className='dodo-icon '>
-                <use xlinkHref={`#${props.name}`}/>
+            <svg className={classnames("dodo-icon", className)}
+                 {...resetProps}>
+                <use xlinkHref={`#${name}`}/>
             </svg>
         </span>
     );
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,22 @@
-import * as React from 'react'
-import * as ReactDom from 'react-dom'
+import * as React from "react";
+import * as ReactDom from "react-dom";
 
 //import Button from './button'
-import Icon from './icon/icon'
+import Icon from "./icon/icon";
+
+const fn: React.MouseEventHandler = (e) => {
+    console.log(111);
+    console.log(e.target);
+};
+const mouse: React.MouseEventHandler = (e) => {
+    console.log(111);
+    console.log(e.target);
+};
 
 ReactDom.render(
     <div>
-        <Icon name="wechat" />,
-        <Icon name="qq" />,
+        <Icon name="wechat" className={'lll'} onClick={fn}/>
+        <Icon name="qq" onClick={fn} onMouseEnter={mouse}/>
     </div>,
-    document.getElementById('root')
-)
+    document.getElementById("root")
+);
